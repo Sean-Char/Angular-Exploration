34a3961f3b1a633f327c714fe56b903cb8b8da6f
Sean-Char <scharoutanh@gmail.com>
2018-08-21 22:10:20 -0500

    Creating A Component

---
+++ src/app/components/user/user.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-user',
+  templateUrl: './user.component.html',
+  styleUrls: ['./user.component.css'],
+  // styles: [`
+  //   h2 {
+  //     color: green
+  //   }
+  // `]
+  // template: '<h2>John Doe</h2>'
+})
+
+export class UserComponent {
+
+}
