6c449f186d03dd0a8bac94341b2bb813c075e515
Sean-Char <scharoutanh@gmail.com>
2018-08-22 19:23:43 -0500

    Generating Components & OnInit

--- src/app/components/user/user.component.ts
+++ src/app/components/user/user.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { User } from '../../models/User';
 
 @Component({
@@ -7,12 +7,16 @@ import { User } from '../../models/User';
   styleUrls: ['./user.component.css']
 })
 
-export class UserComponent {
+export class UserComponent implements OnInit {
   // Properties
   user: User;
 
   // Methods
   constructor() {
+   
+  }
+
+  ngOnInit() {
     this.user = {
       firstName: 'John',
       lastName: 'Wick',
@@ -24,5 +28,6 @@ export class UserComponent {
       }
     }
   }
+
 }
 
